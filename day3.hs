8913f735d6d3d6cd029b2fa04885412529b3d909
Jakob Löw <jakob@m4gnus.de>
2017-12-03 13:27:50 +0100

    rework spiralCityBlock into a function for calculating the coordinates and one for the city block to allow implementation of part two

--- day3.hs
+++ day3.hs
@@ -1,14 +1,25 @@
 import Data.List
+import Data.Maybe
 import Data.Ord
 
-spiralCityBlock n   = distanceY + distanceX
+spiralCoordinates n = coords
     where
-        num         = ceiling $ sqrt $ fromIntegral n
-        rowNum      = num + 1 - (num `mod` 2)
-        rowStart    = (rowNum - 2)^2 + 1
-        row         = [rowStart .. (rowNum^2)]
-        rowLength   = length row
-        index       = n - rowStart
-        centers     = [rowLength * i `div` 8 - 1 | i <- [1,3,5,7]]
-        distanceX   = minimum $ map (abs . (index-)) centers
-        distanceY   = rowNum `div` 2
+        num             = ceiling $ sqrt $ fromIntegral n
+        rowNum          = num + 1 - (num `mod` 2)
+        rowStart        = (rowNum - 2)^2 + 1
+        row             = [rowStart .. (rowNum^2)]
+        rowLength       = length row
+        index           = n - rowStart
+        centers         = [rowLength * i `div` 8 - 1 | i <- [1,3,5,7]]
+        closestCenter   = minimumBy (comparing $ abs . (index-)) centers
+        centerIndex     = fromJust $ elemIndex closestCenter centers
+        distanceMidde   = rowNum `div` 2
+        coords          = case centerIndex of
+            0           -> (distanceMidde, index - closestCenter)
+            1           -> (closestCenter - index, distanceMidde)
+            2           -> (-distanceMidde, closestCenter - index)
+            3           -> (index - closestCenter, -distanceMidde)
+
+spiralCityBlock n       = abs x + abs y
+    where
+        (x, y)          = spiralCoordinates n
