f4edb7ada4d73f32c9e952f91bf8b6ac366b6a25
Jakob Löw <jakob@m4gnus.de>
2017-12-03 11:36:41 +0100

    make stuff less weird

--- day2.hs
+++ day2.hs
@@ -2,9 +2,9 @@ import Control.Monad
 
 checksum f                  = sum . map (f . (map read) . words) . lines
 
-checksum1                   = checksum (liftM2 (-) maximum minimum)
+checksum1                   = checksum (\x -> maximum x - minimum x)
 
-checksum2                   = checksum firstEvenDivision 
+checksum2                   = checksum firstEvenDivision
     where
-        isEvenDiv (x,y)     = y == 0 && x /= 1
-        firstEvenDivision   = fst . head . filter isEvenDiv . join . (map =<< flip (map . divMod))
+        isEvenDiv (x, y)    = y == 0 && x /= 1
+        firstEvenDivision x = fst $ head $ filter isEvenDiv $ concat $ map (\y -> map (divMod y) x) x
