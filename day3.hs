a34215350e5e6263c97ef2a04abe1eb71ffd0314
Jakob Löw <jakob@m4gnus.de>
2017-12-03 13:36:16 +0100

    add the infinite list of sums of near elements in a spiral (needed for part two)

--- day3.hs
+++ day3.hs
@@ -23,3 +23,12 @@ spiralCoordinates n = coords
 spiralCityBlock n       = abs x + abs y
     where
         (x, y)          = spiralCoordinates n
+
+spiralStress            = map snd spiralStressWithCoords
+spiralStressWithCoords  = ((0,0),1) : [spiralStressN n | n <- [2..]]
+spiralStressN n         = (self, sum parts)
+    where
+        self@(sx, sy)   = spiralCoordinates n
+        previous        = take (n - 1) spiralStressWithCoords
+        getCoords c     = fromMaybe 0 $ lookup c previous
+        parts           = [getCoords (sx + x, sy + y) | x <- [-1,0,1], y <- [-1,0,1]]
