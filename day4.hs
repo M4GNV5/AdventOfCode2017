9cd42b119de3a5fdca954a7ccf35ae3e1aa140b3
M4GNV5 <eragonjml@googlemail.com>
2017-12-04 11:30:18 +0100

    make the code nicer and calculate both parts (still pretty inefficient though)

--- day4.hs
+++ day4.hs
@@ -1,8 +1,14 @@
 import Data.List
 
-isUnique x = (length x) == (length $ nub x)
-isLetterUnique x = (length x) == (length $ nub $ (map sort) $ x)
+isUnique x          = (length x) == (length $ nub x)
+isLetterUnique x    = (length x) == (length $ nub $ (map sort) $ x)
 
 main = do
-    str <- readFile "input_day4.txt"
-    putStrLn $ show $ length $ filter atMostOneAnagram $ filter isUnique $ map words $ lines str
+    str             <- readFile "input_day4.txt"
+    let input       = map words $ lines str
+
+    putStr "Part 1: "
+    putStrLn $ show $ length $ filter isUnique input
+
+    putStr "Part 2: "
+    putStrLn $ show $ length $ filter isLetterUnique input
