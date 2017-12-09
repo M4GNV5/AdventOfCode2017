1ce02740da69bee82e645366de4e7861b19abd9b
Jakob Löw <jakob@m4gnus.de>
2017-12-09 16:25:46 +0100

    apparently are there no ! outside of garbage

--- day9.hs
+++ day9.hs
@@ -1,9 +1,7 @@
-data State = Content | Garbage | IgnoreC | IgnoreG deriving(Show)
+data State = Content | Garbage | Ignore deriving(Show)
 
-parseStep (IgnoreC, level, score, count) _      = (Content, level, score, count)
-parseStep (IgnoreG, level, score, count) _      = (Garbage, level, score, count)
-parseStep (Content, level, score, count) '!'    = (IgnoreC, level, score, count)
-parseStep (Garbage, level, score, count) '!'    = (IgnoreG, level, score, count)
+parseStep (Ignore, level, score, count) _       = (Garbage, level, score, count)
+parseStep (Garbage, level, score, count) '!'    = (Ignore, level, score, count)
 parseStep (Content, level, score, count) '{'    = (Content, level + 1, score, count)
 parseStep (Content, level, score, count) '}'    = (Content, level - 1, score + level, count)
 parseStep (Content, level, score, count) '<'    = (Garbage, level, score, count)
@@ -13,7 +11,7 @@ parseStep state _                               = state
 
 main = do
     input                                       <- readFile "input_day9.txt"
-    let (_, _, _, score, count)                 = foldl parseStep (Content, 0, 0, 0) input
+    let (_, _, score, count)                    = foldl parseStep (Content, 0, 0, 0) input
 
     putStrLn $ "Part 1: " ++ (show score)
     putStrLn $ "Part 2: " ++ (show count)
