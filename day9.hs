846b6679227ded42ba784ebb30b825be38511e02
Jakob Löw <jakob@m4gnus.de>
2017-12-09 15:12:05 +0100

    one less element in the tuple but two states more

--- day9.hs
+++ day9.hs
@@ -1,17 +1,19 @@
-data State = Content | Garbage | Ignore deriving(Show)
+data State = Content | Garbage | IgnoreC | IgnoreG deriving(Show)
 
-parseStep (Ignore, prev, level, score, count) _     = (prev, Ignore, level, score, count)
-parseStep (prev, _, level, score, count) '!'        = (Ignore, prev, level, score, count)
-parseStep (Content, _, level, score, count) '{'     = (Content, Ignore, level + 1, score, count)
-parseStep (Content, _, level, score, count) '}'     = (Content, Ignore, level - 1, score + level, count)
-parseStep (Content, _, level, score, count) '<'     = (Garbage, Ignore, level, score, count)
-parseStep (Garbage, _, level, score, count) '>'     = (Content, Ignore, level, score, count)
-parseStep (Garbage, _, level, score, count) _       = (Garbage, Ignore, level, score, count + 1)
-parseStep state _                                   = state
+parseStep (IgnoreC, level, score, count) _      = (Content, level, score, count)
+parseStep (IgnoreG, level, score, count) _      = (Garbage, level, score, count)
+parseStep (Content, level, score, count) '!'    = (IgnoreC, level, score, count)
+parseStep (Garbage, level, score, count) '!'    = (IgnoreG, level, score, count)
+parseStep (Content, level, score, count) '{'    = (Content, level + 1, score, count)
+parseStep (Content, level, score, count) '}'    = (Content, level - 1, score + level, count)
+parseStep (Content, level, score, count) '<'    = (Garbage, level, score, count)
+parseStep (Garbage, level, score, count) '>'    = (Content, level, score, count)
+parseStep (Garbage, level, score, count) _      = (Garbage, level, score, count + 1)
+parseStep state _                               = state
 
 main = do
-    input                                           <- readFile "input_day9.txt"
-    let (_, _, _, score, count)                     = foldl parseStep (Content, Ignore, 0, 0, 0) input
+    input                                       <- readFile "input_day9.txt"
+    let (_, _, _, score, count)                 = foldl parseStep (Content, 0, 0, 0) input
 
     putStrLn $ "Part 1: " ++ (show score)
     putStrLn $ "Part 2: " ++ (show count)
